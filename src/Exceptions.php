20d506ca21aea87a80b9001b895b1b6070941a1b
Marcus Andersson <mackilanu@gmail.com>
2019-07-08 16:15:16 +0200

    Adding exception classes

---
+++ src/Exceptions.php
@@ -0,0 +1,15 @@
+<?php
+namespace Mackilanu\Autho;
+
+/**
+ * Class ValidationException
+ * @package Mackilanu\Autho
+ */
+class ValidationException extends \Exception {}
+
+/**
+ * Class AuthenticationException
+ * @package Mackilanu\Autho
+ */
+class AuthenticationException extends \Exception {}
+
